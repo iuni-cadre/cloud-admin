7a857e42552bee22e7289349ee1ffd7ecde280ef
cpelikan <cpelikan@iu.edu>
2021-08-25 15:13:38 -0400

    adding deployment scripts and crontab

--- aws/start_cluster/start_cluster.py
+++ aws/start_cluster/start_cluster.py
@@ -77,6 +77,8 @@ def poll_queue():
                 # start the cluster
                 try:
                     logger.info(command)
+                    logger.info(python_venv_path)
+                    logger.info(script_path)
                     p = Popen([python_venv_path, script_path] + command.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
                     output, err = p.communicate(b"input data that is passed to subprocess' stdin")
                     rc = p.returncode
